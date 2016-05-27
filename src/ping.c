40d888e0c7e1037ed50297ebb3980868123c5efd
Kronos3 <dovakhiin1359@gmail.com>
2016-05-26 22:22:36 -0400

    Create ping structs

---
+++ src/ping.c
@@ -0,0 +1,35 @@
+/*
+ * ping.c
+ * 
+ * Copyright 2016 Andrei Tumbar <atuser@Kronos>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include "ping.h"
+
+int address_valid(ip *__ip)
+{
+  mstring  ipaddr;
+  ipaddr = __ip->s_address;
+  
+  struct sockaddr_in sa;
+  int result = inet_pton(AF_INET, ipaddr, &(sa.sin_addr));
+  return result != 0;
+}
