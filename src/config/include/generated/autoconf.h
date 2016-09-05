ab5a1c7fc33ad1f15d3f045beb99c2dd3366edf9
Kronos3 <dovakhiin1359@gmail.com>
2016-09-05 01:15:05 -0400

    Added kconfig API and kconfig

---
+++ src/config/include/generated/autoconf.h
@@ -0,0 +1,12 @@
+/*
+ *
+ * Automatically generated file; DO NOT EDIT.
+ * Minux Terminal Configuration
+ *
+ */
+#define CONFIG_CP_RESOLV_CONF 1
+#define CONFIG_GENTOO_RSYNC "rsync://rsync.gentoo.org/gentoo-portage"
+#define CONFIG_NETWORK 1
+#define CONFIG_ROOT_DIR "/mnt/target"
+#define CONFIG_DHCP 1
+#define CONFIG_GENTOO_PKG 1
---
+++ src/kconfig.c
@@ -0,0 +1,40 @@
+/*
+ * kconfig.c
+ * 
+ * Copyright 2016 Andrei Tumbar <atuser@Kronos>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+#ifndef __MINUX_KCONFIG_H__
+#define __MINUX_KCONFIG_H__
+
+#include <stdio.h>
+
+typedef struct _configvar configvar;
+typedef struct _config config;
+
+struct _configvar
+{
+  char     full_str[128]; // With CONFIG_
+  char     name[128]; // Without CONFIG_
+  
+  char     type;
+};
+
+#endif
